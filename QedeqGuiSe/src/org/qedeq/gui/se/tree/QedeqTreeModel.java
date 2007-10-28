237be4f50d334062ce51a24c11155ff54345c58d
Michael Meyling <mime@qedeq.org>
2007-10-28 21:47:08 +0000

    events that lead to tree changes are now handled in extra threads (see SwingUtilities)
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1060 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/gui/se/tree/QedeqTreeModel.java
+++ QedeqGuiSe/src/org/qedeq/gui/se/tree/QedeqTreeModel.java
@@ -21,6 +21,7 @@ package org.qedeq.gui.se.tree;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.SwingUtilities;
 import javax.swing.event.TreeModelListener;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
@@ -48,7 +49,6 @@ public final class QedeqTreeModel extends DefaultTreeModel implements ModuleEven
      */
     private final Map module2Path = new HashMap();
 
-
     public QedeqTreeModel(final TreeNode root) {
         super(root);
     }
@@ -58,17 +58,6 @@ public final class QedeqTreeModel extends DefaultTreeModel implements ModuleEven
      */
     public QedeqTreeModel() {
         this(new QedeqTreeNode(new ModuleElement(), true));
-/*
-        final PmiiTreeNode root = (PmiiTreeNode) getRoot();
-        final PmiiTreeNode axioms = new PmiiTreeNode(
-            new ModuleElement("Axioms", "Module 4",
-            ModuleElement.ATOM), false);
-        final PmiiTreeNode definitions = new PmiiTreeNode(
-            new ModuleElement("Definitions", "Module 4",
-            ModuleElement.ATOM), false);
-        root.add(axioms);
-        root.add(definitions);
-*/
     }
 
     /*  (non-Javadoc)
@@ -79,66 +68,70 @@ public final class QedeqTreeModel extends DefaultTreeModel implements ModuleEven
         super.addTreeModelListener(listener);
     }
 
-    /* (non-Javadoc)
-     * @see com.meyling.principia.module.ModuleListener#addModule(
-     * com.meyling.principia.module.ModuleProperties)
-     */
     public void addModule(final ModuleProperties prop) {
-        final QedeqTreeNode root = (QedeqTreeNode) getRoot();
-        final QedeqTreeNode node = new QedeqTreeNode(prop, false);
-        Trace.trace(this, "addModule", "calls insertNodeInto=" + node);
-        module2Path.put(prop.getAddress(), new TreePath(node.getPath()));
-        for (int i = 0; i < root.getChildCount(); i++) {
-            final QedeqTreeNode n = (QedeqTreeNode) root.getChildAt(i);
-            final ModuleProperties p = (ModuleProperties) n.getUserObject();
-            if (p.getModuleAddress().getFileName().compareTo(prop.getModuleAddress().getFileName())
-                    > 0) {
-                insertNodeInto(node, root, i);
-                return;
+        Runnable addModule = new Runnable() {
+            public void run() {
+                final QedeqTreeNode root = (QedeqTreeNode) getRoot();
+                final QedeqTreeNode node = new QedeqTreeNode(prop, false);
+                Trace.trace(this, "addModule", "calls insertNodeInto=" + node);
+                module2Path.put(prop.getAddress(), new TreePath(node.getPath()));
+                for (int i = 0; i < root.getChildCount(); i++) {
+                    final QedeqTreeNode n = (QedeqTreeNode) root.getChildAt(i);
+                    final ModuleProperties p = (ModuleProperties) n.getUserObject();
+                    if (p.getModuleAddress().getFileName().compareTo(prop.getModuleAddress()
+                            .getFileName()) > 0) {
+                        insertNodeInto(node, root, i);
+                        return;
+                    }
+                }
+                insertNodeInto(node, root, root.getChildCount());
             }
-        }
-        insertNodeInto(node, root, root.getChildCount());
+        };
+
+        SwingUtilities.invokeLater(addModule);
+
     }
 
-
-    /* (non-Javadoc)
-     * @see com.meyling.principia.module.ModuleListener#stateChanged(
-     * com.meyling.principia.module.ModuleProperties)
-     */
     public void stateChanged(final ModuleProperties prop) {
-        final TreePath path = (TreePath) module2Path.get(prop.getAddress());
-        if (path == null) {
-            throw new IllegalArgumentException("unknown property");
-        }
-        final QedeqTreeNode node = (QedeqTreeNode) path.getLastPathComponent();
-        ModuleProperties sameProp = (ModuleProperties) node.getUserObject();
-        if (!prop.equals(sameProp)) {
-            throw new IllegalStateException("should not happen");
-        }
-        Trace.trace(this, "stateChanged", "nodeChanged=" + node);
-        this.nodeChanged(node);
+        Runnable stateChanged = new Runnable() {
+            public void run() {
+                final TreePath path = (TreePath) module2Path.get(prop.getAddress());
+                if (path == null) {
+                    throw new IllegalArgumentException("unknown property");
+                }
+                final QedeqTreeNode node = (QedeqTreeNode) path.getLastPathComponent();
+                ModuleProperties sameProp = (ModuleProperties) node.getUserObject();
+                if (!prop.equals(sameProp)) {
+                    throw new IllegalStateException("should not happen");
+                }
+                Trace.trace(this, "stateChanged", "nodeChanged=" + node);
+                nodeChanged(node);
+            }
+        };
+
+        SwingUtilities.invokeLater(stateChanged);
     }
 
-
-    /* (non-Javadoc)
-     * @see com.meyling.principia.module.ModuleListener#addModule(
-     * com.meyling.principia.module.ModuleProperties)
-     */
     public void removeModule(final ModuleProperties prop) {
-        final TreePath path = (TreePath) module2Path.get(prop.getAddress());
-        if (path == null) {
-            throw new IllegalArgumentException("unknown property");
-        }
-        final QedeqTreeNode node = (QedeqTreeNode) path.getLastPathComponent();
-        ModuleProperties sameProp = (ModuleProperties) node.getUserObject();
-        if (!prop.equals(sameProp)) {
-            throw new IllegalStateException("should not happen");
-        }
-        module2Path.remove(prop.getAddress());
-        Trace.trace(this, "removeModule", "nodeRemoved=" + node);
-        this.removeNodeFromParent(node);
-    }
+        Runnable removeModule = new Runnable() {
+            public void run() {
+                final TreePath path = (TreePath) module2Path.get(prop.getAddress());
+                if (path == null) {
+                    throw new IllegalArgumentException("unknown property");
+                }
+                final QedeqTreeNode node = (QedeqTreeNode) path.getLastPathComponent();
+                ModuleProperties sameProp = (ModuleProperties) node.getUserObject();
+                if (!prop.equals(sameProp)) {
+                    throw new IllegalStateException("should not happen");
+                }
+                module2Path.remove(prop.getAddress());
+                Trace.trace(this, "removeModule", "nodeRemoved=" + node);
+                removeNodeFromParent(node);
+            }
+        };
 
+        SwingUtilities.invokeLater(removeModule);
+    }
 
     public TreePath getSelectionPath(final ModuleProperties prop) {
         final TreePath path = (TreePath) module2Path.get(prop.getAddress());
