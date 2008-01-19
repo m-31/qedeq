09715a403694a8f865d40777fa6c0c9cc5249467
Michael Meyling <mime@qedeq.org>
2008-01-19 21:04:41 +0000

    new logical
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1389 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelSe/src/org/qedeq/kernel/bo/control/ClassOperatorAlreadyExistsException.java
@@ -0,0 +1,57 @@
+/* $Id: LoadRequiredModuleException.java,v 1.1 2007/12/21 23:33:46 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2007,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.bo.control;
+
+import org.qedeq.kernel.bo.module.ModuleContext;
+import org.qedeq.kernel.bo.module.ModuleDataException;
+
+
+/**
+ * A class operator was defined twice.
+ *
+ * @version $Revision: 1.1 $
+ * @author  Michael Meyling
+ */
+public class ClassOperatorAlreadyExistsException extends ModuleDataException {
+
+    /**
+     * Constructor.
+     *
+     * @param   errorCode   Error code of this message.
+     * @param   message     Error message.
+     * @param   context     Error location.
+     */
+    public ClassOperatorAlreadyExistsException(final int errorCode, final String message,
+            final ModuleContext context) {
+        super(errorCode, message, context);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param   errorCode   Error code of this message.
+     * @param   message     Error message.
+     * @param   context     Error location.
+     * @param   reference   Reference location.
+     */
+    public ClassOperatorAlreadyExistsException(final int errorCode, final String message,
+            final ModuleContext context, final ModuleContext reference) {
+        super(errorCode, message, context, reference);
+    }
+
+}
---
+++ QedeqKernelSe/src/org/qedeq/kernel/bo/control/ModuleConstantsExistenceChecker.java
@@ -0,0 +1,105 @@
+/* $Id: QedeqBoFormalLogicChecker.java,v 1.5 2007/12/21 23:33:46 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2007,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.bo.control;
+
+import org.qedeq.kernel.bo.logic.DefaultExistenceChecker;
+import org.qedeq.kernel.bo.module.ModuleDataException;
+import org.qedeq.kernel.bo.module.ModuleProperties;
+import org.qedeq.kernel.bo.module.ModuleReferenceList;
+
+
+/**
+ * Checks if all formulas of a QEDEQ module are well formed.
+ *
+ * @version $Revision: 1.5 $
+ * @author  Michael Meyling
+ */
+public class ModuleConstantsExistenceChecker extends DefaultExistenceChecker {
+
+    /** QEDEQ module properties. */
+    private final ModuleProperties prop;
+
+    /**
+     * Constructor.
+     *
+     * @param   prop                QEDEQ module properties object.
+     * @throws  ModuleDataException Referenced QEDEQ modules are already inconsistent: they doesn't
+     *          mix.
+     */
+    public ModuleConstantsExistenceChecker(final ModuleProperties prop)
+            throws  ModuleDataException {
+        super();
+        this.prop = prop;
+        initialCheck();
+    }
+
+    public void initialCheck() throws ModuleDataException {
+        clear();
+        boolean identityOperatorExists = false;
+        boolean classOperatorExists = false;
+        final ModuleReferenceList list = prop.getRequiredModules();
+        String identityOperator = null;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.getModuleProperties(i).getExistenceChecker().equalityOperatorExists()) {
+                if (identityOperatorExists) {
+                    // FIXME mime 20089116: check if both definitions are the same (Module URL ==)
+                    throw new IdentityOperatorAlreadyExistsException(123476,
+                        "identity operator already defined", list.getModuleContext(i));
+                }
+                identityOperatorExists = true;
+                identityOperator = list.getLabel(i) + "."
+                    + list.getModuleProperties(i).getExistenceChecker().getIdentityOperator();
+            }
+            if (list.getModuleProperties(i).getExistenceChecker().classOperatorExists()) {
+                if (classOperatorExists) {
+                    // FIXME mime 20089116: check if both definitions are the same (Module URL ==)
+                    throw new ClassOperatorAlreadyExistsException(123478,
+                        "class operator already defined", list.getModuleContext(i));
+                }
+                classOperatorExists = true;
+            }
+        }
+        setIdentityOperatorDefined(identityOperatorExists, identityOperator);
+        setClassOperatorExists(classOperatorExists);
+    }
+
+    public boolean predicateExists(final String name, final int arguments) {
+        final int external = name.indexOf(".");
+        if (external < 0) {
+            return super.predicateExists(name, arguments);
+        }
+        final String label = name.substring(0, external);
+        final ModuleReferenceList ref = prop.getRequiredModules();
+        final ModuleProperties newProp = ref.getModuleProperties(label);
+        final String shortName = name.substring(external + 1);
+        return newProp.getExistenceChecker().predicateExists(shortName, arguments);
+    }
+
+    public boolean functionExists(final String name, final int arguments) {
+        final int external = name.indexOf(".");
+        if (external < 0) {
+            return super.functionExists(name, arguments);
+        }
+        final String label = name.substring(0, external);
+        final ModuleReferenceList ref = prop.getRequiredModules();
+        final ModuleProperties newProp = ref.getModuleProperties(label);
+        final String shortName = name.substring(external + 1);
+        return newProp.getExistenceChecker().functionExists(shortName, arguments);
+    }
+
+}
