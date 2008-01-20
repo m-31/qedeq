f60cb81e766276ef96d590e7bd7126c04b63453b
Michael Meyling <mime@qedeq.org>
2008-01-20 20:34:44 +0000

    new kernel version number
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1434 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelSe/src/org/qedeq/kernel/context/KernelContext.java
+++ QedeqKernelSe/src/org/qedeq/kernel/context/KernelContext.java
@@ -46,7 +46,7 @@ public final class KernelContext implements Kernel {
     private static final Class CLASS = KernelContext.class;
 
     /** Version of this kernel. */
-    private static final String KERNEL_VERSION = "0.03.07";
+    private static final String KERNEL_VERSION = "0.03.08";
 
     /** Version dependent directory of this kernel. */
     private static final String KERNEL_VERSION_DIRECTORY = KERNEL_VERSION.replace('.', '_');
