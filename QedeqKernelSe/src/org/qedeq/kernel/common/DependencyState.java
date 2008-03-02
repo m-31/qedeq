5678ca5d226703b2aa51bb6f068a85fe1e5c351b
Michael Meyling <mime@qedeq.org>
2008-03-02 09:44:22 +0000

    not needed states removed
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1521 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelSe/src/org/qedeq/kernel/common/DependencyState.java
+++ QedeqKernelSe/src/org/qedeq/kernel/common/DependencyState.java
@@ -42,17 +42,6 @@ public final class DependencyState {
         DependencyStateDescriptions.STATE_STRING_LOADING_REQUIRED_MODULES_FAILED, true,
         DependencyStateDescriptions.STATE_CODE_LOADING_REQUIRED_MODULES_FAILED);
 
-    /** Loading required modules of required modules. */
-    public static final DependencyState STATE_LOADING_REQUIRED_REQUIREMENTS = new DependencyState(
-        DependencyStateDescriptions.STATE_STRING_LOADING_REQUIRED_REQUIREMENTS, false,
-        DependencyStateDescriptions.STATE_CODE_LOADING_REQUIRED_REQUIREMENTS);
-
-    /** Loading required modules of required modules failed. */
-    public static final DependencyState STATE_LOADING_REQUIRED_REQUIREMENTS_FAILED
-            = new DependencyState(
-        DependencyStateDescriptions.STATE_STRING_LOADING_REQUIRED_REQUIREMENTS_FAILED, true,
-        DependencyStateDescriptions.STATE_CODE_LOADING_REQUIRED_REQUIREMENTS_FAILED);
-
     /** Completely loaded. */
     public static final DependencyState STATE_LOADED_REQUIRED_MODULES = new DependencyState(
         DependencyStateDescriptions.STATE_STRING_LOADED_REQUIRED_MODULES, false,
