00322d6565947714d83fb49356dd2dd29a0883a7
Michael Meyling <mime@qedeq.org>
2008-05-01 23:58:46 +0000

    new
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1711 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqGuiSe/src/org/qedeq/gui/se/control/ErrorSelectionListener.java
@@ -0,0 +1,38 @@
+/* $Id: ErrorSelectionListener.java,v 1.6 2008/04/15 05:14:03 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2008,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.gui.se.control;
+
+import org.qedeq.kernel.common.SourceFileException;
+
+/**
+ * Listener for error selection events.
+ *
+ * @version $Revision: 1.6 $
+ * @author  Michael Meyling
+ */
+public interface ErrorSelectionListener {
+
+    /**
+     * This error was selected.
+     *
+     * @param   errorNumber Selected error number. Starts with 0.
+     * @param   sf          Selected error.
+     */
+    public void selectError(int errorNumber, SourceFileException sf);
+
+}
---
+++ QedeqGuiSe/src/org/qedeq/gui/se/control/ErrorSelectionListenerList.java
@@ -0,0 +1,90 @@
+/* $Id: ErrorSelectionListenerList.java,v 1.8 2008/04/15 05:18:26 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2008,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.gui.se.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.qedeq.kernel.common.SourceFileException;
+import org.qedeq.kernel.trace.Trace;
+
+
+/**
+ * This class organizes the listening to error selection events.
+ *
+ * LATER mime 20080415: must this be a singleton?
+ *
+ * @version $Revision: 1.1 $
+ * @author  Michael Meyling
+ */
+public final class ErrorSelectionListenerList implements ErrorSelectionListener {
+
+    /** This class. */
+    private static final Class CLASS = ErrorSelectionListenerList.class;
+
+    /** The one and only instance. */
+    private static ErrorSelectionListenerList instance = new ErrorSelectionListenerList();
+
+    /** The loggers. */
+    private List listeners = new ArrayList();
+
+    /**
+     * Get instance of master listener.
+     *
+     * @return  singleton
+     */
+    public static final ErrorSelectionListenerList getInstance() {
+        return instance;
+    }
+
+    /**
+     * Don't use me outside of this class.
+     */
+    private ErrorSelectionListenerList() {
+    }
+
+    /**
+     * Add listener.
+     *
+     * @param   log Add this listener.
+     */
+    public final void addListener(final ErrorSelectionListener list) {
+        listeners.add(list);
+    }
+
+    /**
+     * Remove listener.
+     *
+     * @param   log Remove this listener.
+     */
+    public final void removeListener(final ErrorSelectionListener list) {
+        listeners.remove(list);
+    }
+
+    public void selectError(final int errorNumber, final SourceFileException sf) {
+        for (int i = 0; i < listeners.size(); i++) {
+            try {   // we don't know if the ModuleEventListener is free of programming errors...
+                ((ErrorSelectionListener) listeners.get(i)).selectError(errorNumber, sf);
+            } catch (RuntimeException e) {
+                Trace.fatal(CLASS, this, "selectError",
+                    "ErrorSelectionListener throwed RuntimeException", e);
+            }
+        }
+    }
+
+}
