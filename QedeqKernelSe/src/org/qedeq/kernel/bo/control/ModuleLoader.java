63d49bb608f7d81c34d9ad4bd02e26a75ff46655
Michael Meyling <mime@qedeq.org>
2008-05-10 21:38:04 +0000

    refactored loading
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1755 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelSe/src/org/qedeq/kernel/bo/control/ModuleLoader.java
+++ QedeqKernelSe/src/org/qedeq/kernel/bo/control/ModuleLoader.java
@@ -55,11 +55,10 @@ public interface ModuleLoader {
      * @param   prop        Module properties.
      * @param   localFile   Load XML file from tbis location.
      * @return  Loaded Qedeq.
-     * @throws  ModuleFileNotFoundException    Local file was not found.
      * @throws  SourceFileExceptionList    Module could not be successfully loaded.
      */
     public Qedeq loadLocalModule(KernelQedeqBo prop, final File localFile)
-            throws ModuleFileNotFoundException, SourceFileExceptionList;
+            throws SourceFileExceptionList;
 
     /**
      * Get area in source file for QEDEQ module context.
