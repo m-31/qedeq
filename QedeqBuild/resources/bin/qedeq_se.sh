8d4aa47181c5c6fb3f0a3f23efc7283b56980089
Michael Meyling <mime@qedeq.org>
2008-07-07 08:53:55 +0000

    added
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1920 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqBuild/resources/bin/qedeq_se.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# this script starts the standalone GUI front end of *Hilbert II*
+
+# change to directory of script directory
+cd $(dirname $0)
+
+# set http proxy (if any)
+QEDEQ_PROXY=
+# if you need a proxy please remove the leading # of the following line and adjust the settings
+# QEDEQ_PROXY="-DproxySet=true  -DproxyHost=myProxyHost -DproxyPort=myProxyPort" 
+
+# set java classpath
+QEDEQ_CP=config:lib/qedeq_gui_se.jar:lib/qedeq_kernel_se.jar:lib/xercesImpl.jar:lib/xml-apis.jar:lib/commons-logging-1.1.jar:lib/log4j-1.2.14.jar:lib/forms-1.1.0.jar:lib/looks-2.1.4.jar
+
+# set splash screen (activate for java 1.6)
+# set QEDEQ_SPLASH=-splash:qedeq.png
+
+#start program
+# if "java" is not in the path you must add the JRE bin dictory to your path or
+# fill in the full path to the java executable
+java $QEDEQ_PROXY $QEDEQ_SPLASH -cp $QEDEQ_CP org.qedeq.gui.se.main.QedeqMainFrame "$@"
+
+
+
+
---
+++ QedeqBuild/resources/jar/readme.txt
@@ -0,0 +1,13 @@
+Hilbert II, Version @QEDEQ_VERSION@ (@QEDEQ_CODENAME@), Build @QEDEQ_BUILD_ID@
+
+This open source project wants to present mathematical knowledge in a 
+formal correct form. The knowledge is organized in QEDEQ modules which could be 
+spread over the whole world wide web.
+
+Look at 
+    http://sourceforge.net/projects/pmii/
+    http://www.qedeq.org/
+to get the latest source code and information
+
+All project files are published under the GPL see the file
+"gpl.txt" for further details.
