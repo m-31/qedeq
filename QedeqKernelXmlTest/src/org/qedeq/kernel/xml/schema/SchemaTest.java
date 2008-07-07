a28871a73593d6675d44e2e87cce135d58c67d8b
Michael Meyling <mime@qedeq.org>
2008-07-07 08:47:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@1915 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelXmlTest/src/org/qedeq/kernel/xml/schema/SchemaTest.java
@@ -0,0 +1,59 @@
+/* $Id: SchemaTest.java,v 1.10 2008/03/27 05:12:45 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2008,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+package org.qedeq.kernel.xml.schema;
+
+import java.io.File;
+
+import org.qedeq.base.test.QedeqTestCase;
+
+/**
+ * This class tests the XML schema files under <code>org.qedeq.kernel.xml.schema</code>.
+ *
+ * @version $Revision: 1.10 $
+ * @author    Michael Meyling
+ */
+public class SchemaTest extends QedeqTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test XSLT transformation.
+     *
+     * @throws  Exception   Something bad happened.
+     */
+    public void testXml() throws Exception {
+        try {
+            org.apache.xalan.xslt.Process.main(new String[] {
+//                "-EDUMP", "-TT",   // for debugging
+                "-IN", getFile("qedeq.xml").toString(), "-XSL",
+                    getFile("qedeq.xsl").toString(),
+                    "-OUT", (new File(getOutdir(), "qedeq.html")).toString(), "-HTML"});
+        } catch (RuntimeException t) {
+            t.printStackTrace();
+            throw t;
+        } catch (Error t) {
+            t.printStackTrace();
+            throw t;
+        }
+    }
+}
---
+++ QedeqKernelXmlTest/src/org/qedeq/kernel/xml/test/KernelXmlTestSuite.java
@@ -0,0 +1,67 @@
+/* $Id: KernelTestSuite.java,v 1.21 2008/03/27 05:12:46 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2008,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.xml.test;
+
+import junit.framework.Test;
+
+import org.qedeq.base.test.QedeqTestSuite;
+import org.qedeq.kernel.xml.dao.GenerateXmlTest;
+import org.qedeq.kernel.xml.parser.QedeqParserTest;
+import org.qedeq.kernel.xml.schema.SchemaTest;
+import org.qedeq.kernel.xml.tracker.KernelXmlTrackerTestSuite;
+
+/**
+ * Run all tests for the project.
+ *
+ * @version $Revision: 1.21 $
+ * @author Michael Meyling
+ */
+public class KernelXmlTestSuite extends QedeqTestSuite {
+
+    /**
+     * Get a new <code>KernelTestSuite</code>.
+     *
+     * @return Test.
+     */
+    public static Test suite() {
+        return new KernelXmlTestSuite();
+    }
+
+    /**
+     * Constructor.
+     */
+    protected KernelXmlTestSuite() {
+        this(true, false);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param   withTest    Execute test methods.
+     * @param   withPest    Execute pest methods.
+     */
+    public KernelXmlTestSuite(final boolean withTest, final boolean withPest) {
+        super(withTest, withPest);
+        addTestSuite(GenerateXmlTest.class);
+
+        addTestSuite(QedeqParserTest.class);
+        addTestSuite(SchemaTest.class);
+        addTest(KernelXmlTrackerTestSuite.suite());
+    }
+
+}
