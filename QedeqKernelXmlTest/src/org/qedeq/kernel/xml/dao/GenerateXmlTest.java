7e0befc655a19c6cc6e2f64cbb5eab8d79773437
Michael Meyling <mime@qedeq.org>
2009-06-12 20:46:22 +0000

    debug code included
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2061 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelXmlTest/src/org/qedeq/kernel/xml/dao/GenerateXmlTest.java
+++ QedeqKernelXmlTest/src/org/qedeq/kernel/xml/dao/GenerateXmlTest.java
@@ -80,6 +80,8 @@ public final class GenerateXmlTest extends QedeqTestCase {
         final File xmlFile = new File(dir, xml);
         final File destination = new File(destinationDirectory, xml + "_").getAbsoluteFile();
         Xml2Xml.generate(xmlFile, destination);
+//        assertEquals(IoUtility.loadFile(xmlFile.getAbsolutePath(), "ISO-8859-1"), 
+//            IoUtility.loadFile(destination.getAbsolutePath(), "ISO-8859-1"));
         assertEquals(true, IoUtility.compareTextFiles(xmlFile, destination));
     }
 
