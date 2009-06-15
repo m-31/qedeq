7b3a5f403d3e595fc655e09ae20086f36b497661
Michael Meyling <mime@qedeq.org>
2009-06-15 13:08:35 +0000

    some more tests
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2070 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqBaseTest/src/org/qedeq/base/io/IoUtilityTest.java
+++ QedeqBaseTest/src/org/qedeq/base/io/IoUtilityTest.java
@@ -18,8 +18,14 @@
 package org.qedeq.base.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
 
 import org.qedeq.base.test.QedeqTestCase;
+import org.qedeq.base.utility.EqualsUtility;
 
 /**
  * Test {@link org.qedeq.kernel.utility.TextInput}.
@@ -46,18 +52,135 @@ public class IoUtilityTest extends QedeqTestCase {
 
 
     /**
-     * Test createRelativePath(final File orgin, final File next).
+     * Test {@link IoUtility#createRelativePath(final File origin, final File next)}.
      *
      * @throws Exception
      */
     public void testCreateRelativePath() throws Exception {
-        assertEquals("local", IoUtility.createRelativePath(new File("."), new File ("local")));
+        assertEquals("local", IoUtility.createRelativePath(new File("."), new File("local")));
         assertEquals("text", IoUtility.createRelativePath(new File("/local/data"),
-            new File ("/local/data/text")));
+            new File("/local/data/text")));
         assertEquals("../../green", IoUtility.createRelativePath(new File("/local/data"),
-            new File ("/green")));
+            new File("/green")));
         assertEquals("../green", IoUtility.createRelativePath(new File("/local/data"),
-            new File ("/local/green")));
+            new File("/local/green")));
+    }
+
+    /**
+     * Test {@link IoUtility#loadFileBinary(File)}.
+     *
+     * @throws Exception
+     */
+    public void testLoadFileBinary() throws Exception {
+        final File file = new File("IoUtilityTestLoadBinary.bin");
+        if (file.exists()) {
+            assertTrue(file.delete());
+        }
+        final OutputStream out = new FileOutputStream(file);
+        try {
+            for (int i = 0; i < 256; i++) {
+                out.write(i);
+            }
+            for (int i = 0; i < 256; i++) {
+                out.write(i);
+            }
+        } finally {
+            out.close();
+        }
+        final byte [] loaded = IoUtility.loadFileBinary(file);
+        assertEquals(512, loaded.length);
+        for (int i = 0; i < 256; i++) {
+            assertEquals((byte) i, loaded[i]);
+        }
+        for (int i = 0; i < 256; i++) {
+            assertEquals((byte) i, loaded[i]);
+        }
+        file.delete();
+    }
+
+    /**
+     * Test {@link IoUtility#saveFileBinary(File, byte[])}.
+     *
+     * @throws Exception
+     */
+    public void testSaveFileBinary() throws Exception {
+        final File file = new File("IoUtilityTestLoadBinary.bin");
+        if (file.exists()) {
+            assertTrue(file.delete());
+        }
+        final byte data[] = new byte[512];
+        for (int i = 0; i < 512; i++) {
+            data[i] = (byte) i;
+        }
+        IoUtility.saveFileBinary(file, data);
+        final InputStream in = new FileInputStream(file);
+        try {
+            for (int i = 0; i < 256; i++) {
+                assertEquals(i, in.read());
+            }
+            for (int i = 0; i < 256; i++) {
+                assertEquals(i, in.read());
+            }
+            assertEquals(-1, in.read());
+        } finally {
+            in.close();
+        }
+        file.delete();
+    }
+
+    /**
+     * Test {@link IoUtility#saveFileBinary(File, byte[])}.
+     *
+     * @throws Exception
+     */
+    public void testLoadAndSaveFileBinary() throws Exception {
+        final File file = new File("IoUtilityTestLoadBinary.bin");
+        if (file.exists()) {
+            assertTrue(file.delete());
+        }
+        Random random = new Random(1001);
+        final byte data[] = new byte[100000];
+        for (int i = 0; i < 512; i++) {
+            data[i] = (byte) i;
+        }
+        for (int i = 512; i < data.length; i++) {
+            data[i] = (byte) random.nextInt();
+        }
+        IoUtility.saveFileBinary(file, data);
+        EqualsUtility.equals(data, IoUtility.loadFileBinary(file));
+        file.delete();
+    }
+
+    /**
+     * Test {@link IoUtility#compareFilesBinary(File, File)}.
+     *
+     * @throws Exception
+     */
+    public void testCompareFile() throws Exception {
+        final File file1 = new File("IoUtilityTestLoadBinary1.bin");
+        if (file1.exists()) {
+            assertTrue(file1.delete());
+        }
+        final File file2 = new File("IoUtilityTestLoadBinary2.bin");
+        if (file2.exists()) {
+            assertTrue(file2.delete());
+        }
+        Random random = new Random(1001);
+        final byte data[] = new byte[1025];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (byte) random.nextInt();
+        }
+        IoUtility.saveFileBinary(file1, data);
+        IoUtility.saveFileBinary(file2, data);
+        assertTrue(IoUtility.compareFilesBinary(file1, file2));
+        data[1000] += 1;
+        IoUtility.saveFileBinary(file2, data);
+        assertFalse(IoUtility.compareFilesBinary(file1, file2));
+        data[999] -= 1;
+        IoUtility.saveFileBinary(file2, data);
+        assertFalse(IoUtility.compareFilesBinary(file1, file2));
+        file1.delete();
+        file2.delete();
     }
 
 }
