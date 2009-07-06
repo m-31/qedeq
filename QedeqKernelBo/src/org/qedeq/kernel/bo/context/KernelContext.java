1dd317ef2e94a8aa158b99c6afaa90402e638885
Michael Meyling <mime@qedeq.org>
2009-07-06 19:53:02 +0000

    0.03.11 -> 0.03.12
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2119 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/context/KernelContext.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/context/KernelContext.java
@@ -51,7 +51,7 @@ public final class KernelContext implements KernelProperties, KernelState, Kerne
     private static final Class CLASS = KernelContext.class;
 
     /** Version of this kernel. */
-    private static final String KERNEL_VERSION = "0.03.11";
+    private static final String KERNEL_VERSION = "0.03.12";
 
     /** Version dependent directory of this kernel. */
     private static final String KERNEL_VERSION_DIRECTORY = KERNEL_VERSION.replace('.', '_');
