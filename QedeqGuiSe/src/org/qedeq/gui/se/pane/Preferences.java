7362d24096b79559ce7eaf55e28765e655384da8
Michael Meyling <mime@qedeq.org>
2009-09-23 08:05:54 +0000

    changed header
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2178 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/gui/se/pane/Preferences.java
+++ QedeqGuiSe/src/org/qedeq/gui/se/pane/Preferences.java
@@ -1,20 +1,19 @@
-// $Id: Preferences.java,v 1.5 2008/07/26 07:57:44 m31 Exp $
-//
-// This file is part of the program suite "Principia Mathematica II"
-// which is a working prototype for the main project:
-// "Hilbert II" - http://www.qedeq.org
-//
-// Copyright 2001-2004  Michael Meyling <mime@qedeq.org>.
-//
-// "Principia Mathematica II" is free software; you can redistribute
-// it and/or modify it under the terms of the GNU General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+/* $Id: Preferences.java,v 1.5 2008/07/26 07:57:44 m31 Exp $
+ *
+ * This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2009,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
 
 package org.qedeq.gui.se.pane;
 
