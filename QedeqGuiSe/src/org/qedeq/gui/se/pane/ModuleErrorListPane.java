e7e6c9934c7f2e9726feff6b9a321d1f5d5d6a7f
Michael Meyling <mime@qedeq.org>
2010-03-04 20:01:03 +0000

    checkstyle warning: empty command removed: ";"
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2288 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/gui/se/pane/ModuleErrorListPane.java
+++ QedeqGuiSe/src/org/qedeq/gui/se/pane/ModuleErrorListPane.java
@@ -293,7 +293,7 @@ public class ModuleErrorListPane extends JPanel implements ModuleEventListener {
                 }
             };
             SwingUtilities.invokeLater(removeModule);
-        };
+        }
     }
 
 }
