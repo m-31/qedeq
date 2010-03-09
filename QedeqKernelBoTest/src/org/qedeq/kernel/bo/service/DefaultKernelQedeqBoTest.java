c8534125bc08d799dc3fa73b924821fbb089a0a2
Michael Meyling <mime@qedeq.org>
2010-03-09 22:59:19 +0000

    ModuleLabels now also in BO trier
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2322 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBoTest/src/org/qedeq/kernel/bo/service/DefaultKernelQedeqBoTest.java
+++ QedeqKernelBoTest/src/org/qedeq/kernel/bo/service/DefaultKernelQedeqBoTest.java
@@ -27,12 +27,12 @@ import org.qedeq.kernel.bo.module.DefaultExistenceChecker;
 import org.qedeq.kernel.bo.module.InternalKernelServices;
 import org.qedeq.kernel.bo.module.KernelModuleReferenceList;
 import org.qedeq.kernel.bo.module.KernelQedeqBo;
+import org.qedeq.kernel.bo.module.ModuleLabels;
 import org.qedeq.kernel.bo.module.QedeqFileDao;
 import org.qedeq.kernel.common.DefaultSourceFileExceptionList;
 import org.qedeq.kernel.common.DependencyState;
 import org.qedeq.kernel.common.LoadingState;
 import org.qedeq.kernel.common.ModuleAddress;
-import org.qedeq.kernel.common.ModuleLabels;
 import org.qedeq.kernel.common.SourceFileExceptionList;
 import org.qedeq.kernel.dto.module.QedeqVo;
 
