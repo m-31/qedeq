d8acdf1738593535a4c06c1fc1cc40bc9bdafeb8
Michael Meyling <mime@qedeq.org>
2010-08-12 22:21:56 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2618 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/gui/se/util/GuiHelper.java
+++ QedeqGuiSe/src/org/qedeq/gui/se/util/GuiHelper.java
@@ -68,11 +68,11 @@ public final class GuiHelper {
 
     /** Color for line highlighter. */
     private static Color lineHighlighterBackgroundColor = new Color(232, 242,
-            254);
+        254);
 
     /** Color for line highlighter and marked areas. */
     private static Color markedAndHiglightedBackgroundColor = new Color(232,
-            242, 254, 128);
+        242, 254, 128);
 
     /** Color for error text areas. */
     private static Color errorTextBackgroundColor = new Color(180, 206, 255);
@@ -104,12 +104,12 @@ public final class GuiHelper {
         Options.setUseNarrowButtons(options.isUseNarrowButtons());
         Options.setTabIconsEnabled(options.isTabIconsEnabled());
         UIManager.put(Options.POPUP_DROP_SHADOW_ENABLED_KEY, options
-                .isPopupDropShadowEnabled());
+            .isPopupDropShadowEnabled());
         // LATER m31 20100319: we make this now direct in QedeqPane, this line
         // didn't help. Why?
         // we want our disabled TextAreas to look same if not editable
         UIManager.put("TextArea.disabledBackground", UIManager
-                .get("TextArea.background"));
+            .get("TextArea.background"));
 
         // Swing Settings
         LookAndFeel selectedLaf = options.getSelectedLookAndFeel();
@@ -117,7 +117,7 @@ public final class GuiHelper {
             PlasticLookAndFeel.setPlasticTheme(options.getSelectedTheme());
             PlasticLookAndFeel.setTabStyle(options.getPlasticTabStyle());
             PlasticLookAndFeel.setHighContrastFocusColorsEnabled(options
-                    .isPlasticHighContrastFocusEnabled());
+                .isPlasticHighContrastFocusEnabled());
         } else if (selectedLaf.getClass() == MetalLookAndFeel.class) {
             MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
         }
@@ -138,12 +138,10 @@ public final class GuiHelper {
 
     /**
      * Creates a JButton configured for use in a JToolBar.
-     * 
-     * @param iconName
-     *            Name of icon.
-     * @param toolTipText
-     *            Tool tip text.
-     * @return Button.
+     *
+     * @param   iconName    Name of icon.
+     * @param   toolTipText Tool tip text.
+     * @return  Button.
      */
     public static AbstractButton createToolBarButton(final String iconName,
             final String toolTipText) {
@@ -155,39 +153,33 @@ public final class GuiHelper {
 
     /**
      * Creates a JButton configured for use in a JToolBar.
-     * 
-     * @param iconName
-     *            Name of icon.
-     * @param toolTipText
-     *            Tool tip text.
-     * @param action
-     *            Action for this button.
-     * @param keyStroke
-     *            Short cut key.
-     * @return Button.
+     *
+     * @param   iconName    Name of icon.
+     * @param   toolTipText Tool tip text.
+     * @param   action      Action for this button.
+     * @param   keyStroke   Short cut key.
+     * @return  Button.
      */
     public static AbstractButton createToolBarButton(final String iconName,
             final String toolTipText, final ActionListener action,
             final KeyStroke keyStroke) {
         AbstractButton button = createToolBarButton(iconName, toolTipText);
         button.registerKeyboardAction(action, keyStroke,
-                JComponent.WHEN_IN_FOCUSED_WINDOW);
+            JComponent.WHEN_IN_FOCUSED_WINDOW);
         return button;
     }
 
     /**
      * Creates a JToggleButton configured for use in a JToolBar.
-     * 
-     * @param iconName
-     *            Name of icon.
-     * @param toolTipText
-     *            Tool tip text.
-     * @return Radio button.
+     *
+     * @param   iconName    Name of icon.
+     * @param   toolTipText Tool tip text.
+     * @return  Radio button.
      */
     public static AbstractButton createToolBarRadioButton(
             final String iconName, final String toolTipText) {
         final JToggleButton button = new JToggleButton(GuiHelper
-                .readImageIcon(iconName));
+            .readImageIcon(iconName));
         button.setToolTipText(toolTipText);
         button.setFocusable(false);
         return button;
@@ -195,10 +187,9 @@ public final class GuiHelper {
 
     /**
      * Get an icon for given filename postfix.
-     * 
-     * @param filename
-     *            Look for this icon.
-     * @return Icon.
+     *
+     * @param   filename    Look for this icon.
+     * @return  Icon.
      */
     public static ImageIcon readImageIcon(final String filename) {
         URL url = ResourceLoaderUtility.getResourceUrl("images/" + filename);
@@ -207,8 +198,8 @@ public final class GuiHelper {
 
     /**
      * Get background color for highlighting the current line.
-     * 
-     * @return Background color.
+     *
+     * @return  Background color.
      */
     public static Color getLineHighlighterBackgroundColor() {
         return lineHighlighterBackgroundColor;
@@ -216,7 +207,7 @@ public final class GuiHelper {
 
     /**
      * Get background color for error text areas.
-     * 
+     *
      * @return Background color.
      */
     public static Color getErrorTextBackgroundColor() {
@@ -225,7 +216,7 @@ public final class GuiHelper {
 
     /**
      * Get background color for warning text areas.
-     * 
+     *
      * @return Background color.
      */
     public static Color getWarningTextBackgroundColor() {
@@ -234,7 +225,7 @@ public final class GuiHelper {
 
     /**
      * Get background color for marked and highlightted text areas.
-     * 
+     *
      * @return Background color.
      */
     public static Color getCurrentAndMarkedBackgroundColor() {
@@ -244,11 +235,9 @@ public final class GuiHelper {
     /**
      * Paint current line background area with
      * {@link #getLineHighlighterBackgroundColor()}.
-     * 
-     * @param g
-     *            Graphics to use.
-     * @param c
-     *            Text component to work on.
+     *
+     * @param   g   Graphics to use.
+     * @param   c   Text component to work on.
      */
     public static void paintCurrentLineBackground(final Graphics g,
             final JTextComponent c, final Color col) {
@@ -268,10 +257,9 @@ public final class GuiHelper {
 
     /**
      * Adds boarder space and floats panel to the right.
-     * 
-     * @param panel
-     *            Panel to decorate.
-     * @return Panel with more decorations.
+     *
+     * @param   panel   Panel to decorate.
+     * @return  Panel with more decorations.
      */
     public static JComponent addSpaceAndAlignRight(final JPanel panel) {
         JPanel withSpace = new JPanel();
@@ -284,8 +272,8 @@ public final class GuiHelper {
 
     /**
      * Width of horizontal empty border.
-     * 
-     * Return horizontal empty boarder pixel distance.
+     *
+     * @return  Return horizontal empty boarder pixel distance.
      */
     public static int getEmptyBoderPixelsX() {
         return DEFAULT_EMPTY_BORDER_PIXEL_X;
@@ -293,8 +281,8 @@ public final class GuiHelper {
 
     /**
      * Width of vertical empty border.
-     * 
-     * Return vertical empty boarder pixel distance.
+     *
+     * @return  Return vertical empty boarder pixel distance.
      */
     public static int getEmptyBorderPixelsY() {
         return DEFAULT_EMPTY_BORDER_PIXEL_Y;
@@ -302,30 +290,28 @@ public final class GuiHelper {
 
     /**
      * A border that puts extra pixels at the sides and bottom of each pane.
-     * 
-     * @return Border with extra space.
+     *
+     * @return  Border with extra space.
      */
     public static Border getEmptyBorder() {
         return BorderFactory.createEmptyBorder(GuiHelper
-                .getEmptyBorderPixelsY(), GuiHelper.getEmptyBoderPixelsX(),
-                GuiHelper.getEmptyBorderPixelsY(), GuiHelper
-                        .getEmptyBoderPixelsX());
+            .getEmptyBorderPixelsY(), GuiHelper.getEmptyBoderPixelsX(),
+            GuiHelper.getEmptyBorderPixelsY(), GuiHelper.getEmptyBoderPixelsX());
     }
 
     /**
      * A border that puts extra pixels at the sides and bottom of each pane.
-     * 
-     * @return Border with extra space.
+     *
+     * @return  Border with extra space.
      */
     public static Border getEmptyBorderStackable() {
         return BorderFactory.createEmptyBorder(GuiHelper
-                .getEmptyBorderPixelsY() / 2, GuiHelper.getEmptyBoderPixelsX(),
-                GuiHelper.getEmptyBorderPixelsY() / 2, GuiHelper
-                        .getEmptyBoderPixelsX());
+            .getEmptyBorderPixelsY() / 2, GuiHelper.getEmptyBoderPixelsX(),
+            GuiHelper.getEmptyBorderPixelsY() / 2, GuiHelper.getEmptyBoderPixelsX());
     }
 
     /**
-     * Caclulate table column width according to contents.
+     * Calculate table column width according to contents.
      *
      * @author  Christian Kaufhold
      * See <a href="http://www.chka.de/swing/table/cell-sizes.html">Calculating initial
@@ -334,7 +320,7 @@ public final class GuiHelper {
      * @param   table   Calculate width for this table.
      */
     public static void calcColumnWidths(JTable table) {
-        JTableHeader header = table.getTableHeader();
+        final JTableHeader header = table.getTableHeader();
         TableCellRenderer defaultHeaderRenderer = null;
         if (header != null) {
             defaultHeaderRenderer = header.getDefaultRenderer();
