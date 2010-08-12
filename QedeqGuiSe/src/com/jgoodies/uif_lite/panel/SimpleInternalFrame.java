6c9b0aa27db4ad3d6296fdcd6ad6d3513d114b0a
Michael Meyling <mime@qedeq.org>
2010-08-12 15:28:30 +0000

    GradientPanel extracted
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2609 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/com/jgoodies/uif_lite/panel/SimpleInternalFrame.java
+++ QedeqGuiSe/src/com/jgoodies/uif_lite/panel/SimpleInternalFrame.java
@@ -431,33 +431,4 @@ public class SimpleInternalFrame extends JPanel {
         }
     }
 
-
-    /**
-     * A panel with a horizontal gradient background.
-     */
-    private static final class GradientPanel extends JPanel {
-
-        private GradientPanel(LayoutManager lm, Color background) {
-            super(lm);
-            setBackground(background);
-        }
-
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (!isOpaque()) {
-                return;
-            }
-            Color control = UIManager.getColor("control");
-            int width  = getWidth();
-            int height = getHeight();
-
-            Graphics2D g2 = (Graphics2D) g;
-            Paint storedPaint = g2.getPaint();
-            g2.setPaint(
-                new GradientPaint(0, 0, getBackground(), width, 0, control));
-            g2.fillRect(0, 0, width, height);
-            g2.setPaint(storedPaint);
-        }
-    }
-
 }
