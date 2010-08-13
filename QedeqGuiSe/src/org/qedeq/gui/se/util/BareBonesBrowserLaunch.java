b866667b7fa9d389c7cf428083fd26f7724d2caa
Michael Meyling <mime@qedeq.org>
2010-08-13 16:25:52 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@2655 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/gui/se/util/BareBonesBrowserLaunch.java
+++ QedeqGuiSe/src/org/qedeq/gui/se/util/BareBonesBrowserLaunch.java
@@ -18,17 +18,28 @@ import org.qedeq.base.utility.YodaUtility;
  * Public Domain Software -- Free to Use as You Like
  * @version 3.1, June 6, 2010
  */
-public class BareBonesBrowserLaunch {
+public final class BareBonesBrowserLaunch {
 
-   static final String[] browsers = { "google-chrome", "firefox", "opera",
+   /** Browser list. */
+   private static final String[] BROWSERS = {"google-chrome", "firefox", "opera",
       "epiphany", "konqueror", "conkeror", "midori", "kazehakase", "mozilla" };
-   static final String errMsg = "Error attempting to launch web browser";
+
+   /** Error message. */
+   private static final String ERR_MSG = "Error attempting to launch web browser";
 
    /**
-    * Opens the specified web page in the user's default browser
+    * Constructor.
+    */
+   private BareBonesBrowserLaunch() {
+       // nothing to do
+   }
+
+   /**
+    * Opens the specified web page in the user's default browser.
+    *
     * @param url A web address (URL) of a web page (ex: "http://www.google.com/")
     */
-   public static void openURL(String url) {
+   public static void openURL(final String url) {
        System.out.println("openURL"); // FIXME remove me
        if (YodaUtility.existsMethod("java.awt.Desktop", "browse", new Class[] {java.net.URI.class})) {
            try {  //attempt to use Desktop library from JDK 1.6+
@@ -39,11 +50,10 @@ public class BareBonesBrowserLaunch {
                return;
                //above code mimicks:  java.awt.Desktop.getDesktop().browse()
           } catch (Exception ignore) {
-              ignore.printStackTrace(System.out);    // FIXME
               //library not available or failed
           }
        }
-       String osName = System.getProperty("os.name");
+       final String osName = System.getProperty("os.name");
        try {
            if (osName.startsWith("Mac OS")) {
                Class.forName("com.apple.eio.FileManager").getDeclaredMethod(
@@ -54,19 +64,20 @@ public class BareBonesBrowserLaunch {
                   "rundll32 url.dll,FileProtocolHandler " + url);
            } else { //assume Unix or Linux
                String browser = null;
-               for (int i = 0; i < browsers.length; i++) {
-                   String b = browsers[i];
+               for (int i = 0; i < BROWSERS.length; i++) {
+                   String b = BROWSERS[i];
                    if (browser == null && Runtime.getRuntime().exec(new String[]
                         {"which", b}).getInputStream().read() != -1) {
-                      Runtime.getRuntime().exec(new String[] {browser = b, url});
+                      Runtime.getRuntime().exec(new String[] {b, url});
+                      browser = b;
                    }
                }
                if (browser == null) {
-                   throw new Exception(StringUtility.toString(browsers));
+                   throw new Exception(StringUtility.toString(BROWSERS));
                }
             }
        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, errMsg + "\n" + e.toString());
+            JOptionPane.showMessageDialog(null, ERR_MSG + "\n" + e.toString());
          }
       }
 
