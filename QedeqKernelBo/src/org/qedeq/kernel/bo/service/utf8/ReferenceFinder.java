419363b9eee5060d975a5dc318f302299b86ea49
Michael Meyling <mime@qedeq.org>
2010-10-28 20:43:45 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@3065 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/service/utf8/ReferenceFinder.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/service/utf8/ReferenceFinder.java
@@ -27,12 +27,12 @@ public interface ReferenceFinder {
 
     /**
      * Return string for reference link.
-     * 
+     *
      * @param   reference       Link to this.
      * @param   subReference    Which has this sub reference. Might be "" but not null.
      * @param   startDelta      Absolute source position start of reference.
      * @param   endDelta        Absolute source position end of reference.
-     * @return
+     * @return  Return reference as string.
      */
     public String getReferenceLink(final String reference, final String subReference,
         final SourcePosition startDelta, final SourcePosition endDelta);
