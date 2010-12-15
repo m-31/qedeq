c30a20e7f713ee2cb96849bc638cebdeb893268d
Michael Meyling <mime@qedeq.org>
2010-12-15 00:13:44 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@3278 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/module/ModuleConstantsExistenceCheckerInterface.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/module/ModuleConstantsExistenceCheckerInterface.java
@@ -1,3 +1,18 @@
+/* This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2010,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
 package org.qedeq.kernel.bo.module;
 
 import org.qedeq.kernel.base.module.FunctionDefinition;
@@ -9,6 +24,11 @@ import org.qedeq.kernel.bo.service.ClassOperatorAlreadyExistsException;
 import org.qedeq.kernel.bo.service.IdentityOperatorAlreadyExistsException;
 import org.qedeq.kernel.common.ModuleContext;
 
+/**
+ * Contains methods for existence checking of various operands.
+ *
+ * @author  Michael Meyling
+ */
 public interface ModuleConstantsExistenceCheckerInterface extends ExistenceChecker {
 
     public boolean predicateExists(final Predicate predicate);
