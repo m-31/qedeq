796a7592ea0d62c8a5b22204d97de6307931e2d4
Michael Meyling <mime@qedeq.org>
2011-01-14 18:58:31 +0000

    refactoring
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@3416 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelBoTest/src/org/qedeq/kernel/bo/service/logic/KernelBoServiceLogicTestSuite.java
@@ -0,0 +1,46 @@
+/* This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2010,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.bo.service.logic;
+
+import junit.framework.Test;
+
+import org.qedeq.base.test.QedeqTestSuite;
+
+/**
+ * Run all junit tests for package org.qedeq.kernel.bo.service.logic
+ *
+ * @author    Michael Meyling
+ */
+public class KernelBoServiceLogicTestSuite extends QedeqTestSuite {
+
+    /**
+     * Get a new <code>KernelVoModuleTestSuite</code>.
+     *
+     * @return  Test.
+     */
+    public static Test suite() {
+        return new KernelBoServiceLogicTestSuite();
+    }
+
+    /**
+     * Constructor.
+     */
+    public KernelBoServiceLogicTestSuite() {
+        super();
+        addTestSuite(QedeqBoFormalLogicCheckerDirectTest.class);
+        addTestSuite(QedeqBoFormalLogicCheckerTest.class);
+    }
+}
