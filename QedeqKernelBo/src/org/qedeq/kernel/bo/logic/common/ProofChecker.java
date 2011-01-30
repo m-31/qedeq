717f35c54ec5100dc98314bd24fd10b8ba5391c7
Michael Meyling <mime@qedeq.org>
2011-01-30 00:58:26 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@3517 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/logic/common/ProofChecker.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/logic/common/ProofChecker.java
@@ -15,22 +15,21 @@
 
 package org.qedeq.kernel.bo.logic.common;
 
-import org.qedeq.kernel.se.base.list.Element;
 import org.qedeq.kernel.se.base.module.FormalProof;
 import org.qedeq.kernel.se.common.ModuleContext;
 
 /**
- * A formula checker can check logical correctness of a formula or term.
+ * A proof checker can check if a formal proof is correct.
  *
  * @author  Michael Meyling
  */
 public interface ProofChecker {
 
     /**
-     * Checks if an {@link Element} is a formula. If there are any errors the returned list
+     * Checks if an formal proof is ok. If there are any errors the returned list
      * (which is always not <code>null</code>) has a size greater zero.
      *
-     * @param   element             Check this element.
+     * @param   proof               Check this formal proof.
      * @param   context             For location information. Important for locating errors.
      * @param   existenceChecker    Existence checker for operators.
      * @return  Collected errors if there are any. Not <code>null</code>.
@@ -38,43 +37,5 @@ public interface ProofChecker {
     public LogicalCheckExceptionList checkProof(final FormalProof proof,
             final ModuleContext context, final ExistenceChecker existenceChecker);
 
-    /**
-     * Checks if an {@link Element} is a formula. All predicates and functions are assumed to exist.
-     * If there are any errors the returned list (which is always not <code>null</code>) has a size
-     * greater zero.
-     * If the existence context is known you should use
-     * {@link #checkFormula(Element, ModuleContext, ExistenceChecker)}.
-     *
-     * @param   element             Check this element.
-     * @param   context             For location information. Important for locating errors.
-     * @return  Collected errors if there are any. Not <code>null</code>.
-     */
-    public LogicalCheckExceptionList checkFormula(final Element element,
-            final ModuleContext context);
-
-    /**
-     * Check if {@link Element} is a term. If there are any errors the returned list
-     * (which is always not <code>null</code>) has a size greater zero.
-     *
-     * @param   element             Check this element.
-     * @param   context             For location information. Important for locating errors.
-     * @param   existenceChecker    Existence checker for operators.
-     * @return  Collected errors if there are any. Not <code>null</code>.
-     */
-    public LogicalCheckExceptionList checkTerm(final Element element,
-            final ModuleContext context, final ExistenceChecker existenceChecker);
-
-    /**
-     * Check if {@link Element} is a term. If there are any errors the returned list
-     * (which is always not <code>null</code>) has a size greater zero.
-     * If the existence context is known you should use
-     * {@link #checkTerm(Element, ModuleContext, ExistenceChecker)}.
-     *
-     * @param   element Check this element.
-     * @param   context For location information. Important for locating errors.
-     * @return  Collected errors if there are any. Not <code>null</code>.
-     */
-    public LogicalCheckExceptionList checkTerm(final Element element,
-            final ModuleContext context);
 
 }
