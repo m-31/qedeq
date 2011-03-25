33c179ed9b7752a6e4da8d8668b70d16c4730f43
Michael Meyling <mime@qedeq.org>
2011-03-25 20:18:23 +0000

    other plugin as default proof finder
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@3932 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/service/DefaultInternalKernelServices.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/service/DefaultInternalKernelServices.java
@@ -54,6 +54,7 @@ import org.qedeq.kernel.bo.module.InternalKernelServices;
 import org.qedeq.kernel.bo.module.KernelQedeqBo;
 import org.qedeq.kernel.bo.module.QedeqFileDao;
 import org.qedeq.kernel.bo.service.logic.FormalProofCheckerPlugin;
+import org.qedeq.kernel.bo.service.logic.MultiProofFinderPlugin;
 import org.qedeq.kernel.bo.service.logic.SimpleProofFinderPlugin;
 import org.qedeq.kernel.bo.service.logic.WellFormedCheckerPlugin;
 import org.qedeq.kernel.se.base.module.Qedeq;
@@ -121,6 +122,7 @@ public class DefaultInternalKernelServices implements ServiceModule, InternalKer
         pluginManager.addPlugin(FormalProofCheckerPlugin.class.getName());
         pluginManager.addPlugin("org.qedeq.kernel.bo.service.heuristic.DynamicHeuristicCheckerPlugin");
         pluginManager.addPlugin(SimpleProofFinderPlugin.class.getName());
+        pluginManager.addPlugin(MultiProofFinderPlugin.class.getName());
         loader.setServices(this);
     }
 
