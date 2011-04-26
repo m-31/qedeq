6bab262bf44c3bfc4d274df08f1313678a3b7a18
Michael Meyling <mime@qedeq.org>
2011-04-26 13:22:54 +0000

    a little bit more elaborated
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4046 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqGuiSe/src/org/qedeq/text/se/main/Main.java
+++ QedeqGuiSe/src/org/qedeq/text/se/main/Main.java
@@ -133,14 +133,101 @@ public final class Main {
         System.err.println("ERROR>>> " + message);
     }
 
+    /**
+     * Main method.
+     *
+     * @param   args    Various parameters. See implementation of {@link #printProgramInformation()}.
+     */
     public static void main(final String[] args) {
+        String qedeq = null;
+
+        if (args.length == 0) {
+            printProgramInformation();
+            return;
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {  // option
+                String option = args[i].substring(1).toLowerCase();
+                if (option.length() <= 0 && i + 1 < args.length) {
+                    option = args[i + 1];
+                }
+                if (option.equals("help") || option.equals("h")
+                        || option.equals("?")) {
+                    printProgramInformation();
+                    return;
+                } else {                    // unknown option
+                    printArgumentError("Unknown option: " + option);
+                    return;
+                }
+            } else {                        // no option, must be directory name
+                if (qedeq != null) {
+                    printArgumentError("only one QEDEQ module can be searched at once.");
+                    return;
+                }
+                qedeq = args[i];
+            }
+        }
+
+        // check parameters
+        if (qedeq == null) {
+            printArgumentError("QEDEQ module must be specified.");
+            return;
+        }
+
+        findProofs(qedeq);
+    }
+
+    /**
+     * Writes calling convention to <code>System.err</code>.
+     */
+    public static void printProgramInformation() {
+        System.err.println("Name");
+        System.err.println("----");
+        System.err.println(Main.class.getName() + " - find simple propositional calculus proofs");
+        System.err.println();
+        System.err.println("Synopsis");
+        System.err.println("--------");
+        System.err.println("[-h] <module url>");
+        System.err.println();
+        System.err.println("Description");
+        System.err.println("-----------");
+        System.err.println("This program finds formal proofs for propositional calculus propositions. You give it");
+        System.err.println("an QEDEQ module URL and the missing formal proofs will be added. When a proof was found");
+        System.err.println("the buffered file (or original if the protocol is \"file\") will be altered.");
+        System.err.println();
+        System.err.println("Options and Parameter");
+        System.err.println("---------------------");
+        System.err.println("-h           writes this text and returns");
+        System.err.println("<module url> URL for QEDEQ module to work on");
+        System.err.println();
+        System.err.println("Example");
+        System.err.println("-------");
+        System.err.println("http://www.qedeq.org/0_04_02/sample/qedeq_sample4.xml");
+        System.err.println();
+    }
+
+    /**
+     * Print argument error to System.err.
+     *
+     * @param   message Message to print.
+     */
+    private static void printArgumentError(final String message) {
+        System.err.println(">>>ERROR reason:");
+        System.err.println(message);
+        System.err.println();
+        System.err.println(">>>Calling convention:");
+        printProgramInformation();
+    }
+
+    public static void findProofs(final String qedeq) {
         // load configuration file
         try {
             QedeqGuiConfig.init(new File(IoUtility.getStartDirectory("qedeq"),
                 "config/org.qedeq.properties"), IoUtility.getStartDirectory("qedeq"));
         } catch (Throwable e) {
             e.printStackTrace();
-            System.out.println("Configuration file not found!");
+            System.err.println("Configuration file not found!");
             System.exit(-1);
             return;
         }
@@ -150,7 +237,7 @@ public final class Main {
             initLog4J(QedeqGuiConfig.getInstance());
         } catch (Throwable e) {
             e.printStackTrace();
-            System.out.println("Initialization of Log4J failed!");
+            System.err.println("Initialization of Log4J failed!");
             System.exit(-2);
             return;
         }
@@ -169,20 +256,20 @@ public final class Main {
                     new DefaultInternalKernelServices(KernelContext.getInstance(), new XmlQedeqFileDao()),
                     QedeqGuiConfig.getInstance());
                 KernelContext.getInstance().startup();
-                final ModuleAddress address = KernelContext.getInstance().getModuleAddress(args[0]);
+                final ModuleAddress address = KernelContext.getInstance().getModuleAddress(qedeq);
                 KernelContext.getInstance().loadModule(address);
                 KernelContext.getInstance().executePlugin(SimpleProofFinderPlugin.class.getName(),
                     address, null);
                 KernelContext.getInstance().shutdown();
             } catch (IOException e) {
-                System.out.println("Application start failed!");
+                System.err.println("Application start failed!");
                 System.exit(-3);
                 return;
             }
 
         } catch (Throwable e) {
-            e.printStackTrace(System.out);
-            System.out.println("Unexpected major failure!");
+            e.printStackTrace();
+            System.err.println("Unexpected major failure!");
             System.exit(-4);
             return;
         }
