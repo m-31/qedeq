f4e9c68ec33248f57a470decb3cee98c533ea40c
Michael Meyling <mime@qedeq.org>
2011-06-07 17:29:45 +0000

    some more tests
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4281 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqBaseTest/src/org/qedeq/base/io/ParametersTest.java
+++ QedeqBaseTest/src/org/qedeq/base/io/ParametersTest.java
@@ -16,13 +16,13 @@
 package org.qedeq.base.io;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 import org.qedeq.base.test.QedeqTestCase;
 
 /**
  * Test {@link Parameters}.
- * FIXME add more tests
  *
  * @author    Michael Meyling
  */
@@ -49,13 +49,24 @@ public class ParametersTest extends QedeqTestCase {
     protected void setUp() throws Exception {
         super.setUp();
         object1 = new Parameters(new HashMap());
-        final TreeMap map2 = new TreeMap();
+        final Map map2 = new TreeMap();
         object2 = new Parameters(map2);
         map2.put("hello", "7");
-        object3 = new Parameters(new HashMap());
-        object4 = new Parameters(map2);
-        object5 = new Parameters(new HashMap());
-        object5 = new Parameters(null);
+        map2.put("holly", new RuntimeException("I am no string"));
+        final Map map3 = new HashMap();
+        map3.put("hint", "12345678901234567890");
+        map3.put("hilt", "" + Integer.MAX_VALUE);
+        object3 = new Parameters(map3);
+        final Map map4 = new TreeMap();
+        map4.put("hum", "true");
+        map4.put("hom", "false");
+        object4 = new Parameters(map4);
+        final Map map5 = new HashMap();
+        object5 = new Parameters(map5);
+        map5.put("hum", new Integer(9));
+        map5.put("hom", "  12  ");
+        map5.put("him", "-13");
+        object6 = new Parameters(null);
     }
 
     protected void tearDown() throws Exception {
@@ -65,11 +76,28 @@ public class ParametersTest extends QedeqTestCase {
     public void testGetBoolean() {
         assertEquals(false, object1.getBoolean("hello"));
         assertEquals(false, object2.getBoolean("hello"));
+        assertEquals(false, object2.getBoolean("holly"));
+        assertEquals(false, object3.getBoolean("hint"));
+        assertEquals(true, object4.getBoolean("hum"));
+        assertEquals(false, object4.getBoolean("hom"));
+    }
+
+    public void testGetBooleanStandard() {
+        assertEquals(true, object1.getBoolean("hello", true));
+        assertEquals(true, object4.getBoolean("hum", false));
+        assertEquals(false, object4.getBoolean("hom", true));
+        assertEquals(false, object3.getBoolean("hint", false));
+        assertEquals(true, object5.getBoolean("hum", true));
     }
 
     public void testGetInt() {
         assertEquals(0, object1.getInt("hello"));
         assertEquals(7, object2.getInt("hello"));
+        assertEquals(0, object5.getInt("hum"));
+        assertEquals(12, object5.getInt("hom"));
+        assertEquals(0, object3.getInt("hint"));
+        assertEquals(2147483647, object3.getInt("hilt"));
+        assertEquals(-13, object5.getInt("him"));
     }
 
     public void testGetIntStandard() {
@@ -77,16 +105,16 @@ public class ParametersTest extends QedeqTestCase {
         assertEquals(7, object2.getInt("hello"));
     }
 
-    public void testGetBooleanStandard() {
-        assertEquals(true, object1.getBoolean("hello", true));
-    }
-
     public void testGetString() {
         assertEquals("", object1.getString("hello"));
+        assertEquals("", object2.getString("holly"));
+        assertEquals("", object5.getString("hum"));
     }
 
     public void testGetStringStandard() {
         assertEquals("hum", object1.getString("hello", "hum"));
+        assertEquals("hom", object2.getString("holly", "hom"));
+        assertEquals("hunter", object5.getString("hum", "hunter"));
     }
 
 
