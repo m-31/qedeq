3b7c6e9cac975b5b316f6e6f68920e19cfb056a3
Michael Meyling <mime@qedeq.org>
2011-07-21 13:29:37 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4454 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelXmlTest/src/org/qedeq/kernel/xml/test/XmlNormalizerHandler.java
+++ QedeqKernelXmlTest/src/org/qedeq/kernel/xml/test/XmlNormalizerHandler.java
@@ -17,7 +17,6 @@ import org.qedeq.base.utility.Enumerator;
 import org.qedeq.base.utility.StringUtility;
 import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
-import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
