ed43fb050992de72694e61bd9e7fdc02cb7a768d
Michael Meyling <mime@qedeq.org>
2011-08-11 04:55:43 +0000

    clover
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4614 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqBaseTest/src/org/qedeq/base/utility/EqualsUtilityTest.java
+++ QedeqBaseTest/src/org/qedeq/base/utility/EqualsUtilityTest.java
@@ -69,4 +69,54 @@ public class EqualsUtilityTest extends QedeqTestCase {
         assertFalse(EqualsUtility.equals(obj2, obj1));
     }
 
+    /**
+     * Test {@link EqualsUtility#equals(int[], int[])}.
+     *
+     * @throws Exception
+     */
+    public void testEqualsIntArray() throws Exception {
+        final int[] obj1 = new int[10];
+        final int[] obj2 = new int[10];
+        final int[] obj3 = new int[9];
+        assertTrue(EqualsUtility.equals(obj1, obj2));
+        assertTrue(EqualsUtility.equals(obj2, obj1));
+        assertTrue(EqualsUtility.equals(obj1, obj1));
+        assertTrue(EqualsUtility.equals(obj2, obj2));
+        assertFalse(EqualsUtility.equals(obj1, null));
+        assertTrue(EqualsUtility.equals((int[]) null, null));
+        assertFalse(EqualsUtility.equals(null, obj1));
+        assertFalse(EqualsUtility.equals(obj2, null));
+        assertFalse(EqualsUtility.equals(null, obj2));
+        assertFalse(EqualsUtility.equals(obj1, obj3));
+        assertFalse(EqualsUtility.equals(obj3, obj2));
+        obj1[5] = 7;
+        assertFalse(EqualsUtility.equals(obj1, obj2));
+        assertFalse(EqualsUtility.equals(obj2, obj1));
+    }
+
+    /**
+     * Test {@link EqualsUtility#equals(Object[], Object[])}.
+     *
+     * @throws Exception
+     */
+    public void testEqualsObjectArray() throws Exception {
+        final Object[] obj1 = new Object[10];
+        final Object[] obj2 = new Object[10];
+        final Object[] obj3 = new Object[9];
+        assertTrue(EqualsUtility.equals(obj1, obj2));
+        assertTrue(EqualsUtility.equals(obj2, obj1));
+        assertTrue(EqualsUtility.equals(obj1, obj1));
+        assertTrue(EqualsUtility.equals(obj2, obj2));
+        assertFalse(EqualsUtility.equals(obj1, null));
+        assertTrue(EqualsUtility.equals((Object[]) null, null));
+        assertFalse(EqualsUtility.equals(null, obj1));
+        assertFalse(EqualsUtility.equals(obj2, null));
+        assertFalse(EqualsUtility.equals(null, obj2));
+        assertFalse(EqualsUtility.equals(obj1, obj3));
+        assertFalse(EqualsUtility.equals(obj3, obj2));
+        obj1[5] = new Integer(7);
+        assertFalse(EqualsUtility.equals(obj1, obj2));
+        assertFalse(EqualsUtility.equals(obj2, obj1));
+    }
+
 }
