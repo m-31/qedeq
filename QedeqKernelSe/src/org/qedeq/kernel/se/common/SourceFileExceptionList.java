9f8af632d159407b12e546f8d89b2f6ae9869509
Michael Meyling <mime@qedeq.org>
2013-01-18 15:39:30 +0000

    DefaultSourceFileExceptionList SourceFileExceptionList
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4735 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelSe/src/org/qedeq/kernel/se/common/SourceFileExceptionList.java
+++ QedeqKernelSe/src/org/qedeq/kernel/se/common/SourceFileExceptionList.java
@@ -15,20 +15,98 @@
 
 package org.qedeq.kernel.se.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.qedeq.base.utility.EqualsUtility;
+
 
 /**
- * List of {@link org.qedeq.kernel.se.common.SourceFileException}s.
+ * Type save {@link org.qedeq.kernel.se.common.SourceFileException} list.
  *
  * @author  Michael Meyling
  */
-public abstract class SourceFileExceptionList extends Exception {
+public class SourceFileExceptionList extends Exception {
+
+    /** List with parse exceptions. */
+    private final List exceptions = new ArrayList();
+
+    /**
+     * Constructor.
+     */
+    public SourceFileExceptionList() {
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param   e   Wrap me.
+     */
+    public SourceFileExceptionList(final SourceFileException e) {
+        add(e);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param   e   Wrap me.
+     */
+    public SourceFileExceptionList(final SourceFileExceptionList e) {
+        if (e != null) {
+            for (int i = 0; i < e.size(); i++) {
+                add(e.get(i));
+            }
+        }
+    }
+
+    /**
+     * Clear list.
+     */
+    public void clear() {
+        exceptions.clear();
+    }
+
+    /**
+     * Add exception.
+     *
+     * @param   e   Exception to add.
+     */
+    public void add(final SourceFileException e) {
+        if (size() == 0) {
+            initCause(e);
+        }
+        if (!exceptions.contains(e)) {
+            exceptions.add(e);
+        }
+    }
+
+    /**
+     * Add exceptions of given list if they are not already included.
+     *
+     * @param   e   Add exceptions of this list.
+     */
+    public void add(final SourceFileExceptionList e) {
+        if (e == null) {
+            return;
+        }
+        for (int i = 0; i < e.size(); i++) {
+            if (size() == 0) {
+                initCause(e.get(i));
+            }
+            if (!exceptions.contains(e.get(i))) {
+                exceptions.add(e.get(i));
+            }
+        }
+    }
 
     /**
      * Get number of collected exceptions.
      *
      * @return  Number of collected exceptions.
      */
-    public abstract int size();
+    public final int size() {
+        return exceptions.size();
+    }
 
     /**
      * Get <code>i</code>-th exception.
@@ -36,20 +114,63 @@ public abstract class SourceFileExceptionList extends Exception {
      * @param   i   Starts with 0 and must be smaller than {@link #size()}.
      * @return  Wanted exception.
      */
-    public abstract SourceFileException get(int i);
-
-    /**
-     * Add exception.
-     *
-     * @param   e   Exception to add.
-     */
-    public abstract void add(SourceFileException e);
+    public final SourceFileException get(final int i) {
+        return (SourceFileException) exceptions.get(i);
+    }
 
     /**
      * Get all exceptions.
      *
      * @return  All exceptions.
      */
-    public abstract SourceFileException[] toArray();
+    public final SourceFileException[] toArray() {
+        return (SourceFileException[]) exceptions.toArray(
+            new SourceFileException[exceptions.size()]);
+    }
+
+    public int hashCode() {
+        int result = 71;
+        for (int i = 0; i < size(); i++) {
+            result = result ^ (get(i) != null ? get(i).hashCode() : 0);
+        }
+        return result;
+    }
+
+    public boolean equals(final Object object) {
+        if (!(object instanceof SourceFileExceptionList)) {
+            return false;
+        }
+        SourceFileExceptionList sfl = (SourceFileExceptionList) object;
+        if (size() != sfl.size()) {
+            return false;
+        }
+        for (int i = 0; i < size(); i++) {
+            if (!EqualsUtility.equals(get(i), sfl.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String getMessage() {
+        final StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < size(); i++) {
+            if (i != 0) {
+                buffer.append("\n");
+            }
+            final SourceFileException e = get(i);
+            buffer.append(i).append(": ");
+            buffer.append(e.toString());
+        }
+        return buffer.toString();
+    }
+
+    public String toString() {
+        final StringBuffer buffer = new StringBuffer();
+        buffer.append(getClass().getName());
+        buffer.append("\n");
+        buffer.append(getMessage());
+        return buffer.toString();
+    }
 
 }
