f14069b2215fb9ecfd854d69764b5addb6cc3367
Michael Meyling <mime@qedeq.org>
2013-01-19 23:23:02 +0000

    more tests
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@4752 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelSeTest/src/org/qedeq/kernel/se/common/SourceFileExceptionTest.java
+++ QedeqKernelSeTest/src/org/qedeq/kernel/se/common/SourceFileExceptionTest.java
@@ -29,6 +29,8 @@ public class SourceFileExceptionTest extends QedeqTestCase {
     private SourceFileException one;
     private SourceFileException two;
     private SourceFileException three;
+    private SourceFileException four;
+    private SourceFileException five;
 
     private final Plugin plugin = new Plugin(){
         public String getPluginDescription() {
@@ -64,7 +66,14 @@ public class SourceFileExceptionTest extends QedeqTestCase {
             "no big problem", new RuntimeException("something other"),
             new SourceArea("test", new SourcePosition(1, 2), new SourcePosition(3, 17)), 
             new SourceArea("toast", new SourcePosition(13, 7), new SourcePosition(14, 19)));
-        
+        four = new SourceFileException(plugin, 17234,
+            "no big problem", null,
+            new SourceArea("test", new SourcePosition(1, 2), new SourcePosition(3, 17)), 
+            new SourceArea("toast", new SourcePosition(13, 7), new SourcePosition(14, 19)));
+        five = new SourceFileException(plugin, 17234,
+            "one big problem", null,
+            new SourceArea("test", SourcePosition.BEGIN, new SourcePosition(3, 17)),
+            new SourceArea("toast", new SourcePosition(13, 7), SourcePosition.BEGIN));
     }
  
     protected void tearDown() throws Exception {
@@ -77,29 +86,90 @@ public class SourceFileExceptionTest extends QedeqTestCase {
     /**
      * Test constructor.
      */
-    public void testConstructor() throws Exception {
+    public void testConstructor() {
+        assertEquals("no big problem; something bad", one.getMessage());
+        assertEquals(4711, one.getErrorCode());
+        assertNull(four.getCause());
         assertEquals(1, three.getSourceArea().getStartPosition().getRow());
         assertEquals(19, three.getReferenceArea().getEndPosition().getColumn());
+        try {
+            new SourceFileException(null, null, null, null);
+            fail("Exception expected");
+        } catch (Exception e) {
+            // ok
+        }
+    }
+
+    public void testGetMessage() {
+        assertEquals("no big problem; something else", two.getMessage());
+        assertEquals("no big problem", four.getMessage());
+    }
+
+    public void testGetCause() {
+        assertEquals("something bad", one.getCause().getMessage());
+    }
+
+    public void testToString() {
+        assertEquals("\t4711: no big problem; something bad", one.toString());
+        assertEquals("test:1:2\n\t17234: no big problem; something other", three.toString());
+    }
+
+    public void testGetDescription() {
+        assertEquals("\t4711: no big problem; something bad", one.getDescription());
+        assertEquals("test:1:2\n\t17234: no big problem; something other", three.getDescription());
+        assertEquals("test:1:2\n\t17234: no big problem", four.getDescription());
+    }
+
+    public void testGetPlugin() {
+        assertEquals(plugin, one.getPlugin());
+    }
+
+    public void testGetSourceArea() {
+        assertEquals(null, one.getSourceArea());
+        assertEquals(new SourceArea("test"), two.getSourceArea());
+    }
+
+    public void testGetRefrenceArea() {
+        assertEquals(null, one.getReferenceArea());
+        assertEquals(new SourceArea("toast"), two.getReferenceArea());
     }
 
     /**
      * Test hash code generation.
      */
-    public void testHashCode() throws Exception {
+    public void testHashCode() {
         assertFalse(one.hashCode() == two.hashCode());
         assertFalse(three.hashCode() == two.hashCode());
+        assertFalse(three.hashCode() == four.hashCode());
+        assertFalse(five.hashCode() == four.hashCode());
     }
 
-    public void testEqualsObject() throws Exception {
+    /**
+     * Test equals method.
+     */
+    public void testEqualsObject() {
         assertEquals(one, one);
         assertEquals(two, two);
         assertEquals(three, three);
+        assertEquals(four, four);
+        assertEquals(five, five);
+        assertFalse(one.equals(null));
+        assertFalse(one.equals(one.toString()));
         assertFalse(one.equals(two));
         assertFalse(one.equals(three));
+        assertFalse(one.equals(four));
+        assertFalse(one.equals(five));
+        assertFalse(two.equals(null));
+        assertFalse(two.equals(two.toString()));
         assertFalse(two.equals(one));
         assertFalse(two.equals(three));
+        assertFalse(two.equals(four));
+        assertFalse(two.equals(five));
+        assertFalse(three.equals(null));
         assertFalse(three.equals(one));
         assertFalse(three.equals(two));
+        assertFalse(three.equals(four));
+        assertFalse(three.equals(five));
     }
 
 
