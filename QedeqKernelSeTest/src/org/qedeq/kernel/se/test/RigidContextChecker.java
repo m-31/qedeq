ea01802f43bfe108e21666439d13d0fb13c6e9e5
Michael Meyling <mime@qedeq.org>
2013-03-05 00:19:09 +0000

    big refactoring
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@5066 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelSeTest/src/org/qedeq/kernel/se/test/RigidContextChecker.java
@@ -0,0 +1,46 @@
+/* This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2013,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.se.test;
+
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.qedeq.base.test.DynamicGetter;
+import org.qedeq.kernel.se.base.module.Qedeq;
+import org.qedeq.kernel.se.common.ModuleContext;
+import org.qedeq.kernel.se.visitor.ContextChecker;
+
+/**
+ * We really try to get the context in the module.
+ *
+ * @author  Michael Meyling
+ */
+public class RigidContextChecker implements ContextChecker {
+
+    public void checkContext(final Qedeq qedeq, final ModuleContext context) {
+        try {
+            DynamicGetter.get(qedeq, context.getLocationWithinModule());
+        } catch (RuntimeException e) {
+            System.err.println(context);
+            throw e;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
