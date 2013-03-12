c4b41ca280eb482882774bedbc9d733b9c64ab40
Michael Meyling <mime@qedeq.org>
2013-03-12 16:48:14 +0000

    work in progress: Locking Mechanism implementation start
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@5070 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/service/ModuleArbiter.java
@@ -0,0 +1,36 @@
+/* This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2013,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.bo.service;
+
+import org.qedeq.kernel.bo.common.ServiceProcess;
+import org.qedeq.kernel.bo.module.KernelQedeqBo;
+
+/**
+ * Get locks for modules.
+ *
+ * @author  Michael Meyling
+ */
+public class ModuleArbiter {
+
+    public void aquireLockForRequiredModules(final ServiceProcess process, final KernelQedeqBo qedeq) {
+        
+    }
+
+    public void unlockRequired(final ServiceProcess process, final KernelQedeqBo qedeq) {
+        
+    }
+
+}
