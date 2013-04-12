8f96c38aab020ab89fbeb88243d522e50ea4c320
Michael Meyling <mime@qedeq.org>
2013-04-12 14:42:57 +0000

    new
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@5198 2dc5e0b7-7910-0410-9abd-fb16afba6634

---
+++ QedeqKernelBoTest/src/org/qedeq/kernel/bo/test/DummyServiceModule.java
@@ -0,0 +1,28 @@
+/* This file is part of the project "Hilbert II" - http://www.qedeq.org
+ *
+ * Copyright 2000-2013,  Michael Meyling <mime@qedeq.org>.
+ *
+ * "Hilbert II" is free software; you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+package org.qedeq.kernel.bo.test;
+
+import org.qedeq.kernel.bo.common.ServiceModule;
+
+public class DummyServiceModule extends DummyKernelServices implements ServiceModule {
+
+    public void startupServices() {
+    }
+
+    public void shutdownServices() {
+    }
+
+}
