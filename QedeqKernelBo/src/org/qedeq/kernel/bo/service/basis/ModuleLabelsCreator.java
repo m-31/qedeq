f64df07a5553194393493a5f3dfbe04c4cb3dfa8
Michael Meyling <mime@qedeq.org>
2014-01-27 04:21:45 +0000

    checkstyle
    git-svn-id: svn://svn.code.sf.net/p/pmii/code/trunk@5533 2dc5e0b7-7910-0410-9abd-fb16afba6634

--- QedeqKernelBo/src/org/qedeq/kernel/bo/service/basis/ModuleLabelsCreator.java
+++ QedeqKernelBo/src/org/qedeq/kernel/bo/service/basis/ModuleLabelsCreator.java
@@ -16,8 +16,6 @@
 package org.qedeq.kernel.bo.service.basis;
 
 import org.qedeq.base.trace.Trace;
-import org.qedeq.kernel.bo.common.Element2Latex;
-import org.qedeq.kernel.bo.common.Element2Utf8;
 import org.qedeq.kernel.bo.module.InternalServiceJob;
 import org.qedeq.kernel.bo.module.KernelQedeqBo;
 import org.qedeq.kernel.bo.module.ModuleLabels;
